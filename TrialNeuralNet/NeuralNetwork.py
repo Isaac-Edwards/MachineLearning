8af48b8246cee360a8b6d10d7c44403d9a515929
Your Name <you@example.com>
2019-07-25 12:11:36 +0100

    Move sigmoid functions into class

--- TrialNeuralNet/NeuralNetwork.py
+++ TrialNeuralNet/NeuralNetwork.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-def sigmoid(x):
-    return 1/(1+np.exp(-x))
-
-def sigmoid_derivative(x):
-    return sigmoid(x)*(1-sigmoid(x))
-
 class NeuralNetwork:
     def __init__(self, x, y):
         self.input = x                                                  # Input 
@@ -14,14 +8,20 @@ class NeuralNetwork:
         self.y = y                                                      # output layer
         self.output = np.zeros(y.shape)                                 # our guess 
 
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+
+    def sigmoid_derivative(x):
+        return sigmoid(x)*(1-sigmoid(x))
+
     def feedforward(self):
         # biases assumed to be 0
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+        self.layer1 = self.sigmoid(np.dot(self.input, self.weights1))
+        self.output = self.sigmoid(np.dot(self.layer1, self.weights2))
 
     def backprop(self):
-        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T, np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T))
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * self.sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T, np.dot(2*(self.y - self.output) * self.sigmoid_derivative(self.output), self.weights2.T))
 
         self.weights1 += d_weights1
         self.weights2 += d_weights2
