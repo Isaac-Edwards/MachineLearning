01246e2b48193dd9cd224bcd47262d1ea4a76365
Your Name <you@example.com>
2019-10-10 18:30:41 +0100

    Moved TabularModel class into new file

--- ArtificialNeuralNetwork/FullANN/FullANN.py
+++ ArtificialNeuralNetwork/FullANN/FullANN.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+from tabular_model import TabularModel
 
 df = pd.read_csv('~/MachineLearning/UdemyData/NYCTaxiFares.csv')
 
@@ -52,40 +53,6 @@ cat_szs = [len(df[col].cat.categories) for col in cat_cols]
 emb_szs = [(size, min(50, (size+1)//2)) for size in cat_szs]
 
 selfembeds = nn.ModuleList([nn.Embedding(ni, nf) for ni,nf in emb_szs])
-
-class TabularModel(nn.Module):
-  def __init__(self, emb_szs, n_cont, out_sz, layers, p=0.5):
-    super().__init__()
-    self.embeds = nn.ModuleList([nn.Embedding(ni, nf) for ni,nf in emb_szs])
-    self.emb_drop = nn.Dropout(p)
-    self.bn_cont = nn.BatchNorm1d(n_cont)
-
-    layerlist = []
-    n_emb = sum((nf for ni,nf in emb_szs))
-    n_in = n_emb + n_cont
-
-    for i in layers:
-        layerlist.append(nn.Linear(n_in,i)) 
-        layerlist.append(nn.ReLU(inplace=True))
-        layerlist.append(nn.BatchNorm1d(i))
-        layerlist.append(nn.Dropout(p))
-        n_in = i
-
-    layerlist.append(nn.Linear(layers[-1],out_sz))
-
-    self.layers = nn.Sequential(*layerlist)
-
-
-  def forward(self, x_cat, x_cont):
-    embeddings = []
-    for i,e in enumerate(self.embeds):
-      embeddings.append(e(x_cat[:,i]))
-    x = torch.cat(embeddings,1)
-    x=self.emb_drop(x)
-    x_cont = self.bn_cont(x_cont)
-    x =torch.cat([x, x_cont], 1)
-    x = self.layers(x)
-    return x
 
 torch.manual_seed(33)
 model = TabularModel(emb_szs, conts.shape[1], 1, [200,100], p=0.4)
